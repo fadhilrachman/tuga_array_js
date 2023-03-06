35f116bcce103260558cef0783b4370faec42bd2
fadhilrachman <99377200+fadhilrachman@users.noreply.github.com>
2023-03-06 12:45:27 +0700

    fixing tugas

--- tugas.js
+++ tugas.js
@@ -1,15 +1,13 @@
-const arr2 = [
-  ["a", "b", "c"],
-  ["d", "e", "f"],
-  ["g", "h", "i"],
-];
+const arr2 = "abcdefghijklmnopqrstuvwxyz";
 
 function multidimensi_array() {
   var arr = [];
+  var k = 0;
   for (var i = 0; i < 3; i++) {
     var row = [];
     for (var j = 0; j < 3; j++) {
-      row.push(arr2[i][j]); // ['x']
+      row.push(arr2[k]);
+      k++;
     }
     arr.push(row);
   }
